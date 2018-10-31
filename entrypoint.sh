22739aa034883d7c306aeb8af6eb1cfdc0b46a47
vagrant <vagrant@localhost.localdomain>
2018-10-31 04:39:27 +0000

    install app first

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash -x
 set -e
 
+/steamcmd/steamcmd.sh +login anonymous +force_install_dir $HOME +app_update 600760 -beta $BETA +quit
+find $HOME -name ".svn" -type d -depth -exec rm -r "{}" \;
+
 mkdir -p $HOME/log
 
 if [ ! -f $HOME/default.ini ]; then
@@ -8,9 +11,6 @@ if [ ! -f $HOME/default.ini ]; then
   # change default password
   sed -i s/RCONPASSWORD=stationeers/RCONPASSWORD=$(pwgen 15 1)/g $HOME/default.ini
 fi
-
-/steamcmd/steamcmd.sh +login anonymous +force_install_dir $HOME +app_update 600760 -beta $BETA +quit
-find $HOME -name ".svn" -type d -depth -exec rm -r "{}" \;
 
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
